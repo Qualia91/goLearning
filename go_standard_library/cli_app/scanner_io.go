7f85a0855919e37d18685d46f84856e3465dc2e6
Nickolas.Wood <{ID}+{username}@users.noreply.github.com>
2021-03-21 19:54:38 +0000

    improved io example

--- go_standard_library/cli_app/scanner_io.go
+++ go_standard_library/cli_app/scanner_io.go
@@ -16,7 +16,17 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		var age int
+		var name string
+
+		n, err := fmt.Sscanf(scanner.Text(), "%s is %d years old", &name, &age)
+		if err != nil {
+			panic(err)
+		}
+
+		if n == 2 {
+			fmt.Printf("%s, %d\n", name, age)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
