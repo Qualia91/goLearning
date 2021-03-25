0f11d42ae47f539672a28943bcdfe8932ea63261
Nickolas.Wood <{ID}+{username}@users.noreply.github.com>
2021-03-25 15:53:06 +0000

    Functions as first class citizens

--- deepdiveintofunctions/main.go
+++ deepdiveintofunctions/main.go
@@ -18,6 +18,18 @@ func main() {
 	sv.IncrementMajor()
 	println(sv.String())
 
+	// annoymous function
+	myFunc := func() {
+		println("Hello")
+	}
+
+	for i := 0; i < 5; i++ {
+		myFunc()
+	}
+
+	returnedFunc := returnFunc()
+
+	println(returnedFunc(2, 3))
 }
 
 // variadic function
@@ -31,4 +43,11 @@ func variadic(values ...float64) {
 func namedReturnVariables() (answer float64, err error) {
 	answer = 1.0
 	return
+}
+
+// return function
+func returnFunc() func(int, int) int {
+	return func(a, b int) int {
+		return a + b
+	}
 }
