a8abf6cc647dbe9ba625ee5afb57acfed9a4ad46
Nickolas.Wood <{ID}+{username}@users.noreply.github.com>
2021-03-27 11:47:49 +0000

    handling http requests

--- creatingwebservices/main.go
+++ creatingwebservices/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"creatingwebservices/product"
+	"net/http"
+)
+
+const apiBasePath = "/api"
+
+func main() {
+	product.SetupRoutes(apiBasePath)
+	http.ListenAndServe(":5000", nil)
+}
