48f99331ea81be312764b229549d7b62a4a3edc2
Nickolas.Wood <{ID}+{username}@users.noreply.github.com>
2021-04-11 11:45:23 +0100

    docs

---
+++ patterns/microservices/api/doc.go
@@ -0,0 +1,31 @@
+/*API based architecture.
+
+Functional requirements:
+1) Loosly coupled
+2) Independently changable
+3) Independently deployable
+4) Support and honor contracts
+5) Technology agnostic API
+6) Statless API
+
+API Architectural Styles
+- Pragmatic REST:
+		Uses HTTP based infrastructure
+		Use uniform resource interface (post, put, get and delete)
+		Calls resource endpoints (www.site.com/a/b/c)
+		Client-Server based (Server knows nothing about client)
+		Stateless
+		Cacheable
+		Layered system
+		Self descriptive method
+- HATEOS
+- RPC
+- SOAP
+
+Patterns:
+- Facade Pattern
+- Proxy Pattern
+- Stateless service pattern
+*/
+
+package api
