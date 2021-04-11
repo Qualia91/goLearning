5d9906732997022d6891d4e91c5d6575db61fbda
Nickolas.Wood <{ID}+{username}@users.noreply.github.com>
2021-04-11 13:00:50 +0100

    docs

--- patterns/microservices/composition/doc.go
+++ patterns/microservices/composition/doc.go
@@ -5,9 +5,9 @@ Communication within the architecture.
 Pattens:
 - Broker: Messages across a broker
 - Aggregate: Aggregates the data from multiple services and returns aggregation
-- Chained:
-- Branch:
-- Proxy:
+- Chained: Chain together services
+- Branch: Combination of everything to branch out one task into multiple services using multiple mechanisms.
+- Proxy: Use a service to delegrate to other services
 */
 
 package composition
