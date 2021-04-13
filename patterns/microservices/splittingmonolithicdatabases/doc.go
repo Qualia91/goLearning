3231ee7a4902cbc9f32b532845b0ce77d4c17da2
Nickolas.Wood <{ID}+{username}@users.noreply.github.com>
2021-04-13 17:04:23 +0100

    docs

--- patterns/microservices/splittingmonolithicdatabases/doc.go
+++ patterns/microservices/splittingmonolithicdatabases/doc.go
@@ -10,6 +10,9 @@ Approach to design:
 
 Patterns:
 - Event driven: Share data changes using events after saving it to local db
+- Event Sourcing: State is a series of changes, not actual state. To get current state, you need to replay all events in correct order.
+- Command Query Response Segregation (C.Q.R.S): Separate command modules from query models (R/W separation)
+	- Databases synced via low level database technologies
 
 */
 
