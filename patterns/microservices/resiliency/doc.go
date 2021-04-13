1b52290ccc3a30fd58c1d90f41044c04cfeaf36e
Nickolas.Wood <{ID}+{username}@users.noreply.github.com>
2021-04-13 21:28:26 +0100

    docs

--- patterns/microservices/resiliency/doc.go
+++ patterns/microservices/resiliency/doc.go
@@ -4,7 +4,10 @@ Patterns:
 - Timeouts
 - Circuit breaker
 - Retry
-- Bulkhead
+- Bulkhead:
+	High level design pattern
+	Seals off failure to seconds of system
+
 
 Approach:
 Design and embrace failure, fail fast and degrade or default functionality.
